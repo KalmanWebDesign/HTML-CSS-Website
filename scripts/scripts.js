e5b30ccab98179ba2924151c06c3727afd8a13ef
Kalman Web Design <166734153+KalmanWebDesign@users.noreply.github.com>
2024-05-10 14:39:23 -0500

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,18 @@
 
+function on() {
+    // display overlay
+    const turnOn = document.getElementById("overlay");
+    turnOn.style.display = "block";
+    // turn off vertical scroll
+    const overflow = document.querySelector("body");
+    overflow.style.overflow = "hidden";
+}
+
+function off() {
+    // display overlay
+    const turnOff = document.getElementById("overlay");
+    turnOff.style.display = "none";
+    // turn off vertical scroll
+    const overflow = document.querySelector("body");
+    overflow.style.overflow = "";
+}
